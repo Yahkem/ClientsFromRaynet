e8e180ffa191466740731de1de7f6da309394d00
Yahkem <Yahkem@github.com>
2020-08-28 02:40:29 +0200

    delete boilerplate

---
+++ ClientsFromRaynet/ClientApp/src/components/ClientDetail.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+export function ClientDetail() {
+  return <div>TODO Detail</div>;
+}
---
+++ ClientsFromRaynet/ClientApp/src/components/ClientList.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+export function ClientList() {
+  return <div>TODO List table etc</div>;
+}
