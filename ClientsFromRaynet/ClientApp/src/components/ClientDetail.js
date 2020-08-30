798a4faf5bae150c152c7934c09b368a4c7f6da2
Yahkem <Yahkem@github.com>
2020-08-30 03:35:22 +0200

    add social media icons

--- ClientsFromRaynet/ClientApp/src/components/ClientDetail.js
+++ ClientsFromRaynet/ClientApp/src/components/ClientDetail.js
@@ -218,6 +218,10 @@ export function ClientDetail({ clientId }) {
   };
 
   console.log('selectedCompany', selectedCompany);
+  const socNetData = (!!selectedCompany && selectedCompany.socialNetworkContact) ?
+    selectedCompany.socialNetworkContact
+    : {};
+  console.log('socNetDatasocNetData', socNetData)
 
   const companyInfo = !!selectedCompany ?
     (<>
@@ -304,6 +308,31 @@ export function ClientDetail({ clientId }) {
 
           <span className="label-client">Narozeniny/výročí:&nbsp;</span>
           <span>{formatDate(selectedCompany.birthday)}</span>
+        </div>
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3 social-row">
+            <div className={"icon-social icon-fb" + (socNetData.facebook ? ' icon-active' : '')}>
+              {socNetData.facebook && <a href={socNetData.facebook }></a>}
+            </div>
+            <div className={"icon-social icon-twitter" + (socNetData.twitter ? ' icon-active' : '')}>
+              {socNetData.twitter && <a href={socNetData.twitter}></a>}
+            </div>
+            <div className={"icon-social icon-linkedin" + (socNetData.linkedin ? ' icon-active' : '')}>
+              {socNetData.linkedin && <a href={socNetData.linkedin}></a>}
+            </div>
+            <div className={"icon-social icon-pinterest" + (socNetData.pinterest ? ' icon-active' : '')}>
+              {socNetData.pinterest && <a href={socNetData.pinterest}></a>}
+            </div>
+            <div className={"icon-social icon-instagram" + (socNetData.instagram ? ' icon-active' : '')}>
+              {socNetData.instagram && <a href={socNetData.instagram}></a>}
+            </div>
+            <div className={"icon-social icon-skype" + (socNetData.skype ? ' icon-active' : '')}>
+              {socNetData.skype && <a href={"skype:" + socNetData.skype + "?chat"}></a>}
+            </div>
+            <div className={"icon-social icon-yt" + (socNetData.youtube ? ' icon-active' : '')}>
+              {socNetData.youtube && <a href={socNetData.youtube}></a>}
+            </div>
+          </div>
         </div>
       </div>
       <hr />
