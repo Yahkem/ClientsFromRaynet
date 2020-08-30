a3177c1c50ce92de897d8b37d894f436524da007
Yahkem <Yahkem@github.com>
2020-08-30 23:02:43 +0200

    add 'login' page

--- ClientsFromRaynet/ClientApp/src/App.js
+++ ClientsFromRaynet/ClientApp/src/App.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router';
 import { Layout } from './components/Layout';
-import { withStore } from 'react-context-hook';
+import { withStore, useStore } from 'react-context-hook';
 import { ClientList } from './components/ClientList';
 
 import './custom.css'
 
 import Modal from 'react-modal';
+import { Login } from './components/Login';
 
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement('#root')
 
-class App extends Component {
+function App(){
+  const [isLoggedIn] = useStore('isLoggedIn');
+
   // Redirect, so that table isn't re-sorted after detail is opened
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/'>
-          <Redirect to="/client/list" />
-        </Route>
-        <Route exact path='/client'>
-          <Redirect to="/client/list" />
-        </Route>
-        <Route path='/client/:clientId' component={ClientList} />
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Route exact path='/'>
+        <Redirect to={isLoggedIn ? "/client/list" : "/login" } />
+      </Route>
+      <Route exact path='/client'>
+        <Redirect to={isLoggedIn ? "/client/list" : "/login"} />
+      </Route>
+      <Route path='/client/:clientId' component={ClientList}>
+        {!isLoggedIn && <Redirect to="/login" />}
+      </Route>
+      <Route exact path='/login' component={Login}>
+        {isLoggedIn && <Redirect to="/client/list" />}
+      </Route>
+    </Layout>
+  );
 }
 
 export default withStore(App);
---
+++ ClientsFromRaynet/ClientApp/src/components/Login.js
@@ -0,0 +1,54 @@
+﻿import { withStore, useStore } from 'react-context-hook';
+import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+
+export function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useStore('isLoggedIn');
+  const [invalidPw, setInvalidPw] = useState(false);
+
+  function hashFunc(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const chr = str.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
+
+  const hashValue = 1417606992;
+  const history = useHistory();
+
+  const onButtonClick = (e) => {
+    const inputValue = document.getElementById('access-password').value;
+    const hashedInput = hashFunc(inputValue);
+
+    if (hashedInput === hashValue) {
+      setInvalidPw(false);
+      setIsLoggedIn(true);
+      history.push('/client');
+    } else {
+      setInvalidPw(true);
+    }
+  };
+
+  return (
+    <form onSubmit={(e) => { e.preventDefault(); e.stopPropagation(); return false; }}>
+      <label htmlFor="access-password">Přístupový kód:&nbsp;</label>
+      <input type="text" id="access-password" onInput={(e) => {
+        console.log('e',e, e.persist());
+      }} />
+      <button
+        onClick={onButtonClick}
+        className="login-btn">
+        Zadat
+      </button>
+      {
+        invalidPw && 
+        <>
+          <br />
+          <span className="text-danger">Zadali jste špatný kód.</span>
+        </>
+      }
+    </form>);
+}
